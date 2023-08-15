4efc82a161ba4512515ffd707d9bd9b6b3d3aa56
Алекс Ш <slayer1649@gmail.com>
2023-08-16 00:39:08 +0300

    x

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-namespace :api do
-  namespace :v1 do
-    resources :tasks
-  end
-end
--- src/api.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "http://localhost:3000/api/v1";
-
-export const fetchTasks = async () => {
-  const response = await fetch(`${BASE_URL}/tasks`);
-  const data = await response.json();
-  return data;
-};
