b88b6ab9a7e7501a695eb1f090e2d028bfc57a0e
Алекс Ш <slayer1649@gmail.com>
2023-08-16 13:43:40 +0300

    update

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validates :description, presence: true
+  validates :due_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
-namespace :api do
-  namespace :v1 do
-    resources :tasks
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, except: [:new, :edit]
+    end
   end
+
+  root to: "static#index" # Adjust to your preferred root route
 end
