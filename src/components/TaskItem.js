46f1d1337b15425605abe8f9fc5bc40152b17fa6
Алекс Ш <slayer1649@gmail.com>
2023-08-16 13:50:32 +0300

    Update TaskItem.js

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,56 +1,40 @@
-.task-item {
-  padding: 16px;
-  margin: 12px 0;
-  border: 1px solid #444444;
-  border-radius: 4px;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-  background-color: #333333;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  transition: background-color 0.2s;
+// src/components/TaskItem.js
+
+import React, { useState } from "react";
+import { updateTask, deleteTask } from "../api";
+import "./TaskItem.css"; // Import the CSS file
+
+function TaskItem({ task, onTaskUpdated, onTaskDeleted }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedTitle, setUpdatedTitle] = useState(task.title);
+  const [updatedDescription, setUpdatedDescription] = useState(task.description);
+
+  const handleEditToggle = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleTaskUpdate = async () => {
+    const updatedTask = {
+      ...task,
+      title: updatedTitle,
+      description: updatedDescription,
+    };
+
+    await updateTask(updatedTask);
+    onTaskUpdated(task.id, updatedTask);
+    setIsEditing(false);
+  };
+
+  const handleTaskDelete = async () => {
+    await deleteTask(task.id);
+    onTaskDeleted(task.id);
+  };
+
+  return (
+    <li className="task-item">
+      {/* ... rest of your component */}
+    </li>
+  );
 }
 
-.task-item:hover {
-  background-color: #444444;
-}
-
-.task-item-content {
-  flex: 1;
-}
-
-.task-item-title {
-  font-size: 1.1rem;
-  font-weight: bold;
-  margin-bottom: 4px;
-  color: #ffffff;
-}
-
-.task-item-description {
-  color: #bbbbbb;
-  margin-bottom: 8px;
-}
-
-.task-item-due-date {
-  font-size: 0.9rem;
-  color: #999999;
-}
-
-.task-item-actions {
-  display: flex;
-  gap: 8px;
-  align-items: center;
-}
-
-.task-item-edit-button,
-.task-item-delete-button {
-  background-color: transparent;
-  border: none;
-  color: #007bff;
-  cursor: pointer;
-}
-
-.task-item-edit-button:hover,
-.task-item-delete-button:hover {
-  color: #0056b3;
-}
+export default TaskItem;
