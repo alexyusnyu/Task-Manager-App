1f5523918a202f132c5844725e62baea4eba0832
Алекс Ш <slayer1649@gmail.com>
2023-08-16 13:41:52 +0300

    Update tasks_controller.rb

--- app/controllers/api/vi/tasks_controller.rb
+++ app/controllers/api/vi/tasks_controller.rb
@@ -1,62 +1,45 @@
-module Api
-  module V1
-    class TasksController < ApplicationController
-      before_action :set_task, only: [:show, :update, :destroy]
+class Api::V1::TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
 
-      def index
-        tasks = Task.all
-        render json: tasks
-      end
+  def index
+    tasks = Task.all
+    render json: tasks
+  end
 
-      def show
-        render json: @task
-      end
+  def show
+    render json: @task
+  end
 
-      def create
-        task = Task.new(task_params)
+  def create
+    task = Task.new(task_params)
 
-        if task.save
-          render json: task, status: :created
-        else
-          render json: task.errors, status: :unprocessable_entity
-        end
-      end
-
-      def update
-        if @task.update(task_params)
-          render json: @task
-        else
-          render json: @task.errors, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @task.destroy
-        head :no_content
-      end
-
-      private
-
-      def set_task
-        @task = Task.find(params[:id])
-      end
-
-      def task_params
-        params.require(:task).permit(:title, :description)
-      end
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors }, status: :unprocessable_entity
     end
   end
-end
 
-def update
-  if @task.update(task_params)
-    render json: @task
-  else
-    render json: @task.errors, status: :unprocessable_entity
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: { errors: @task.errors }, status: :unprocessable_entity
+    end
   end
-end
 
-def destroy
-  @task.destroy
-  head :no_content
+  def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date)
+  end
 end
